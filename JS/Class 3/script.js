fe645c961f870c944e8619cfbb0fda1a527a2310
manojn1106 <manojdeveloper1997@gmail.com>
2024-07-02 07:51:28 +0530

    JS Arrays, Objects, Contionals, loopings

---
+++ JS/Class 3/script.js
@@ -0,0 +1,94 @@
+// Arrays
+
+// let arr = [10, 20, 30, 100, 200, "hello", true]
+// //         #0   #1  #2  #3  #4................
+
+// arr[4] = 2000;
+
+// console.log(arr);
+// console.log(arr[3]);
+
+// // Objects: {key: value} -> JSON
+
+// let obj = {name: "siva", age: 20, dept:"IT", mobile: 9876543211}
+
+// obj.name = "sivakumar"
+
+// console.log(obj);
+// console.log(obj.name);
+
+// Conditional Statments:
+
+/*
+
+    1. if stmt
+    2. if...else
+    3. if..else..if (Ladder if)
+    4. switch case
+
+*/
+
+// let age = "helloo";
+
+// if (age >= 18) {
+//     console.log("Eligible");
+// } else if(age <= 18) {
+//     console.log("Not Eligible");
+// } else {
+//     console.log("Something went wrong");
+// }
+
+// let day = "Thursday";
+
+// switch(day) {
+//     case "Monday":
+//         console.log("It's Monday");
+//         break;
+//     case "Tuesday":
+//         console.log("It's Tuesday");
+//         break;
+//     case "Wednesday":
+//         console.log("It's Wednesday");
+//         break;
+//     case "Thursday":
+//         console.log("It's Thursday");
+//         break;
+//     case "Friday":
+//         console.log("It's Friday");
+//         break;
+//     case "Saturday":
+//         console.log("It's Saturday");
+//         break;
+//     default:
+//         console.log("Happy weekend");
+//         break;
+// }
+
+// Looping Statements:
+
+/*
+
+    for loop
+    while loop
+    do...while
+
+    for in
+    for of
+    forEach
+
+*/
+
+// for(stmt1; stmt2; stmt4){
+
+// ......stmt3........
+
+// }
+
+// stmt1: Initializing value
+// stmt2: condition
+// stmt3: execution
+// stmt4: increment / decrement
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
